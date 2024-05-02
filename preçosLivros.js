209e94b420817a73851f1a4f93bfc71f52f378e9
caahcelliniamaislinda <137504442+caahcelliniamaislinda@users.noreply.github.com>
2024-05-02 09:41:23 -0300

    Add files via upload

---
+++ preçosLivros.js
@@ -0,0 +1,28 @@
+const precosLinvros = [
+    {
+        titulo: "javaScript",
+        preco: 15 
+    },
+    {
+        titulo:"PHP", 
+        preco: 30
+    },
+    {
+        titulo: "java",
+        preco: 50
+    },
+    {
+        titulo: "elixir", 
+        preco: 50
+    },
+    {
+        titulo:"Go",
+        preco: 45
+    },
+    {
+        titulo: "phyton",
+        preco: 20
+    }
+];
+module.exports = precosLivros;
+ 
